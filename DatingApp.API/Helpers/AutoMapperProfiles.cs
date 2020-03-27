bc4062813e09119be707d45e37477a4b79dd83b4
garygagnon <ggagnon@allixo.com>
2020-03-27 16:10:04 -0700

    added photo upload to cloudinary via api

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,8 @@ namespace DatingApp.API.Helpers
                         src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotoForDetailedDto>();
             CreateMap<UserForUpdateDto, User>();
+            CreateMap<Photo, PhotoForReturnDto>();
+            CreateMap<PhotoForCreationDto, Photo>();
         }
     }
 }
